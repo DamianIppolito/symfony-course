d62cdd3049d609364d0fe53721cddada389b16fb
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-10 09:43:06 +0200

    Seccion 8 - 68

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -4,11 +4,19 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use BlogBundle\Entity\User;
 use BlogBundle\Form\UserType;
 
 class UserController extends Controller
 {
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
     public function loginAction(Request $request){
         $authenticationUtils = $this->get("security.authentication_utils");
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -18,27 +26,30 @@ class UserController extends Controller
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
-        if($form->isValid()){
-            $user = new User();
-            $user->setName($form->get("name")->getData());
-            $user->setSurname($form->get("surname")->getData());
-            $user->setEmail($form->get("email")->getData());
-            $user->setPassword($form->get("password")->getData());
-            $user->setRole("ROLE_USER");
-            $user->setImage(null);
+        if($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $user = new User();
+                $user->setName($form->get("name")->getData());
+                $user->setSurname($form->get("surname")->getData());
+                $user->setEmail($form->get("email")->getData());
+                $user->setPassword($form->get("password")->getData());
+                $user->setRole("ROLE_USER");
+                $user->setImage(null);
 
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($user);
-            $flush = $em->flush();
-            if($flush  == null) {
-                $status = "El usuario se ha creado correctamente";
-            }else{
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($user);
+                $flush = $em->flush();
+                if ($flush == null) {
+                    $status = "El usuario se ha creado correctamente";
+                } else {
+                    $status = "El usuario no se ha creado correctamente";
+                }
+            } else {
                 $status = "El usuario no se ha creado correctamente";
             }
-        }else{
-            $status = "El usuario no se ha creado correctamente";
-        }
 
+            $this->session->getFlashBag()->add("status", $status);
+        }
         return $this->render("BlogBundle:User:login.html.twig", array(
             "error" => $error,
             "last_username" => $lastUsername,
