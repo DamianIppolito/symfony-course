00c13779b2befd7e2f8d2c0e40b51b3212f992a8
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-10 11:57:42 +0200

    Seccion 8 - 72

--- src/BlogBundle/Controller/TagController.php
+++ src/BlogBundle/Controller/TagController.php
@@ -17,6 +17,17 @@ class TagController extends Controller
         $this->session = new Session();
     }
 
+    public function indexAction(){
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $tag_repo = $em->getRepository("BlogBundle:Tag");
+        $tags = $tag_repo->findAll();
+
+        return $this->render("BlogBundle:Tag:index.html.twig", array(
+            "tags" => $tags
+        ));
+    }
+
     public function addAction(Request $request){
         $tag = new Tag();
         $form = $this->createForm(TagType::class, $tag);
@@ -25,15 +36,31 @@ class TagController extends Controller
 
         if($form->isSubmitted()) {
             if ($form->isValid()) {
-                $status = "la etiqueta se ha creado correctamente";
-                $messagebox = 'success';
+
+                $em = $this->getDoctrine()->getEntityManager();
+
+                $tag = new Tag();
+                $tag->setName($form->get("name")->getData());
+                $tag->setDescription($form->get("description")->getData());
+
+                $em->persist($tag);
+                $flush = $em->flush();
+                if($flush == null){
+                    $status = "La etiqueta se ha creado correctamente";
+                    $messagebox = 'success';
+                }else{
+                    $status = "Error al añadir la nueva etiqueta";
+                    $messagebox = 'danger';
+                }
             }else{
-                $status = "la etiqueta no se ha creado porque hay fallos de validación";
+                $status = "La etiqueta no se ha creado porque hay fallos de validación";
                 $messagebox = 'danger';
             }
 
             $this->session->getFlashBag()->add("status", $status);
             $this->session->getFlashBag()->add("messagebox", $messagebox);
+
+            return $this->redirectToRoute('blog_index_tag');
         }
 
         return $this->render("BlogBundle:Tag:add.html.twig", array(
