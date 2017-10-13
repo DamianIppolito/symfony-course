a8b5a8c40b66e26fd657798142dddedfdc6397ce
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-10-13 12:28:40 +0200

    Seccion 8 - 79

--- src/BlogBundle/Repository/EntryRepository.php
+++ src/BlogBundle/Repository/EntryRepository.php
@@ -11,6 +11,13 @@ use BlogBundle\Entity\EntryTag;
 
 class EntryRepository extends \Doctrine\ORM\EntityRepository{
 
+    /**
+     * @param null $tags
+     * @param null $title
+     * @param null $category
+     * @param null $user
+     * @param null $entry
+     */
     public function saveEntryTags($tags=null, $title=null, $category=null, $user=null, $entry=null){
 
         $em = $this->getEntityManager();
@@ -25,13 +32,16 @@ class EntryRepository extends \Doctrine\ORM\EntityRepository{
 
         $tags = explode(",",$tags);
         foreach ($tags as $tag){
+            $tag = trim($tag);
             $isset_tag = $tags_repo->findOneBy(array("name" => $tag));
             if(count($isset_tag) == 0){
                 $tag_obj = new Tag();
                 $tag_obj->setName($tag);
                 $tag_obj->setDescription($tag);
-                $em->persist($tag_obj);
-                $flush = $em->flush();
+                if(!empty($tag)) {
+                    $em->persist($tag_obj);
+                    $flush = $em->flush();
+                }
             }
             $tag = $tags_repo->findOneBy(array("name" => $tag));
             $entryTag = new EntryTag();
